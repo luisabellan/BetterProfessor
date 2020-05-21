0cb39ecfa7d7ea503bc6af065c40cadc8871272a
luisabellan <luis.abellan.dev@gmail.com>
2020-05-21 16:45:47 +0200

    created database, added knex-cleaner, added .env file

---
+++ data/migrations/20200521153250_create.js
@@ -0,0 +1,75 @@
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('users', tbl => {
+        tbl.increments()
+        tbl.string('username', 128)
+          .unique()
+          .notNullable()
+          tbl.text('password')
+          .notNullable()
+          tbl.string('name', 128)
+          .notNullable()
+          tbl.string('email', 128)
+          .unique()
+          .notNullable()
+      })
+      .createTable('projects', tbl => {
+        
+        tbl.increments()
+        tbl.string('name', 128)
+          .notNullable()
+        tbl.date('due_date')
+          .notNullable()
+         
+      })
+      .createTable('reminders', tbl => {
+        tbl.increments()
+        
+        tbl.text('message')
+          .notNullable()
+          tbl.date('date')
+          .notNullable
+          tbl.time('time')
+          .notNullable
+        tbl.integer('user_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('users')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE')
+      })
+  
+    .createTable('users_projects', tbl => {
+    tbl.integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      // this table must exist already
+      .inTable('users')
+    tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      // this table must exist already
+      .inTable('projects')
+  
+    // the combination of the two keys becomes our primary key
+    // will enforce unique combinations of ids
+    tbl.primary(['user_id', 'project_id']);
+  });
+    
+  
+    
+    
+  }
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('reminders')
+      .dropTableIfExists('projects')
+      .dropTableIfExists('users')
+      .dropTableIfExists('users_projects')
+  }
+  
