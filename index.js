d0c497c363dd3085cee8f16cc3226aafb8eb6c31
luisabellan <luis.abellan.dev@gmail.com>
2020-05-30 02:39:26 +0200

    project-users endpoints and models tested with insomnia, now writing tests with jest

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./auth/auth-router");
+const welcomeRouter = require("./welcome/welcome-router");
 const usersRouter = require("./users/users-router");
+const projectsUsersRouter = require("./projects-users/projects-users-router");
 const projectsRouter = require("./projects/projects-router");
 const remindersRouter = require("./reminders/reminders-router");
 const restrict = require("./auth/authenticate-middleware");
@@ -24,8 +26,10 @@ server.use(
     origin: "http://localhost:3000",
   })
 );
+server.use("/", welcomeRouter);
 server.use("/api/users",restrict('mentor'), usersRouter);
-server.use("/api/auth", authRouter); 
+server.use("/api/projects-users",restrict('mentor'), projectsUsersRouter);
+server.use("/api/auth", authRouter);
 server.use("/api/projects",restrict('mentor'), projectsRouter);
 server.use("/api/reminders",restrict('mentor'), remindersRouter);
 
---
+++ projects-users/projects-users-model.js
@@ -0,0 +1,62 @@
+const db = require('../data/dbConfig.js');
+
+// // retrives a user by their username
+// function findByUsername(username) {
+//   return db('users').where({ username }).first();
+// }
+
+
+//  return a list of all users in the database.
+
+function getProjectsUsers() {
+    return db('projects_users').select('project_id','user_id',)
+}
+
+// resolves to  Resolve to a single user object (or null)
+
+function findById(project_id) {
+    return  db('projects_users').select('project_id','user_id').where({ project_id }).first();
+}
+async function validateProject(project_id) {
+    findById(project_id)
+      .then((project) => {
+        if (!project) {
+          console.log(user)
+          return res.status(404).json({
+            message: "The project with the specified project id does not exist.",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+// CREATE USER
+async function add(project_user) {
+    await db('projects_users').insert(project_user)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+//UPDATE USER
+async function update(project_id, data) {
+    await db("projects_users").where({ project_id }).first().update(data);
+    return findById(id);
+  }
+
+// DELETE USER
+  function remove(project_id) {
+    return db("projects_users").where({ project_id }).first().delete()
+  }
+
+module.exports = {
+    getProjectsUsers,
+    findById,
+    add,
+    remove,
+    update,
+    validateProject
+}
