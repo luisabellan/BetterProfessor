c9ed7b3d3116c92f43c55375cf79026ef31dad64
luisabellan <luis.abellan.dev@gmail.com>
2020-05-30 04:20:37 +0200

    save

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ server.use((err, req, res, next) => {
   });
 });
 
-// server.listen(port, () => {
-//   console.log(`Running at http://localhost:${port}`);
-// });
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
 
 module.exports = server;
