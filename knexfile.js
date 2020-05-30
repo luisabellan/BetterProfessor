7028abc08a1267fb60d74b4a4f032165284a14d2
luisabellan <luis.abellan.dev@gmail.com>
2020-05-30 14:06:50 +0200

    save

--- knexfile.js
+++ knexfile.js
@@ -32,14 +32,17 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    
+
     pool: {
       min: 2,
       max: 10
