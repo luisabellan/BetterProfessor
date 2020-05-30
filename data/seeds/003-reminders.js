8ed4f18c3f45931d8b481a69357575461fe8be2d
luisabellan <luis.abellan.dev@gmail.com>
2020-05-30 15:35:34 +0200

    save

--- data/seeds/003-reminders.js
+++ data/seeds/003-reminders.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex, Promise) {
   // we want to remove all data before seeding
   // truncate will reset the primary key each time
-  // return knex("reminders")
-  //   .truncate()
-  //   .then(function () {
+   return knex("reminders")
+    .truncate()
+     .then(function () {
       // add data into insert
       return knex("reminders").insert([
         {
