a80b19e534d274fee8092783d8be2270f2644285
luisabellan <luis.abellan.dev@gmail.com>
2020-05-30 16:18:08 +0200

    save

--- data/seeds/004-users-projects.js
+++ data/seeds/004-users-projects.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex, Promise) {
   // we want to remove all data before seeding
   // truncate will reset the primary key each time
-   return knex('projects_users').truncate()
+   return knex('projects_users').del()
      .then(function () {
       // add data into insert
       return knex('projects_users').insert([
