116f0fcc39e2fd58bb3008c7e3ef32cfd2111283
luisabellan <luis.abellan.dev@gmail.com>
2020-05-31 12:50:06 +0200

    commented out production and staging in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,28 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/users.db3',
+      filename: "./data/users.db3",
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds",
     },
     // add the following
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
   },
 
-  staging: {
+  /* staging: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
 
@@ -53,27 +52,26 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  },
+  }, */
 
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3",
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
-     seeds: {
-      directory: './data/seeds',
+    seeds: {
+      directory: "./data/seeds",
     },
     // add the following
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
-
-   }
-}
+  },
+};
