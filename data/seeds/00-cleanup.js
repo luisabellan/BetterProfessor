46d0c813ae832e9b7bde34850840da7322853a45
luisabellan <luis.abellan.dev@gmail.com>
2020-06-01 10:17:04 +0200

    added process.env.DB_ENV !== 'production' || process.env.DB_ENV !== 'staging' to 00-cleanup.js

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,7 +1,7 @@
 const cleaner = require("knex-cleaner");
 
 exports.seed = function (knex) {
-  if (process.env.DB_ENV !== "production") {
+  if (process.env.DB_ENV !== "production" || process.env.DB_ENV !== "staging") {
     return cleaner.clean(knex, {
       mode: "truncate", // resets ids
       ignoreTables: ["knex_migrations", "knex_migrations_lock"], // don't empty migration tables
